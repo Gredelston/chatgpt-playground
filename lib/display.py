31f925a32e8944fde5034981d15f7eafdbc5f76c
Greg Edelston <gredelston@google.com>
2023-03-02 15:18:13 -0700

    Update display to auto-textwrap.

--- lib/display.py
+++ lib/display.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
 
+import textwrap
+
 from . import types
 
 
 def print_message(message: types.Message):
-    print("\n%s\n" % message["content"].strip())
+    text_wrapper = textwrap.TextWrapper(
+        initial_indent='\t',
+        subsequent_indent='\t',
+        tabsize=4,
+    )
+    message_content = message["content"].strip()
+    wrapped_lines = text_wrapper.wrap(message_content)
+    print('\n' + '\n'.join((line for line in wrapped_lines)) + '\n')
