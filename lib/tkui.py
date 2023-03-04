c133f0a70ab56ec48f8c225c593a837955b234ac
Greg Edelston <Greg Edelston>
2023-03-03 18:25:55 -0700

    Send messages via TkUI

--- lib/tkui.py
+++ lib/tkui.py
@@ -1,15 +1,28 @@
 #!/usr/bin/env python3
 
 import tkinter
+from tkinter import scrolledtext
 from tkinter import ttk
 
-class UIProgram:
-    def __init__(self):
+from lib import session
+
+class UI:
+    def __init__(self, chat_session: session.ChatSession, **kwargs) -> None:
+        self._chat_session = chat_session
+
         self._root = tkinter.Tk()
         self._frame = ttk.Frame(self._root, padding=10)
         self._frame.grid()
+
         ttk.Label(self._frame, text="Hello World!").grid(column=0, row=0)
-        ttk.Button(self._frame, text="Quit", command=self._root.destroy).grid(column=1, row=0)
+        self.input_frame = scrolledtext.ScrolledText(self._frame)
+        self.input_frame.grid(column=0, row=1)
+        ttk.Button(self._frame, text="SendMessage", command=self.send_message).grid(column=0, row=2)
+        ttk.Button(self._frame, text="Quit", command=self._root.destroy).grid(column=0, row=3)
 
     def run(self):
         self._root.mainloop()
+
+    def send_message(self):
+        input_content = self.input_frame.get("1.0", "end-1c")
+        self._chat_session.ask(input_content)
--- nira-tk.py
+++ nira-tk.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python3
 
+import datetime
+
+from lib import session
 from lib import tkui
 
 if __name__ == "__main__":
-    tkui.UIProgram().run()
+    today = datetime.datetime.now().strftime("%Y-%m-%d")
+    system_instruction = (
+        "You are Nira, my personal robotic assistant. "
+        + "Answer as concisely as possible.\n"
+        + f"Current date: {today}"
+    )
+    chat_session = session.ChatSession(
+        system_instruction=system_instruction
+    )
+    chat_session.register_command("clear", chat_session.clear_history_except_system)
+    tkui.UI(chat_session).run()
